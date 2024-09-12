12c80d5234a2de4c93c9cf429909450cad20bd10
Christopher Magdy <chris.magdy37@gmail.com>
2024-09-12 07:48:30 -0700

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print("Addition of {0} and {1} is {2}".format(number1, number2, number1 + number2))
+print("Subtraction of {0} and {1} is {2}".format(number1, number2, number1 - number2))
+print("Multiplication of {0} and {1} is {2}".format(number1, number2, number1 * number2))
