e927512eb305d2d27bf0d32fe80568c8fe45ae88
Christopher Magdy <chris.magdy37@gmail.com>
2024-09-13 11:04:44 -0700

    fix spelling mistake

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_income = int(input("Enter your total monthly income: "))
+monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - monthly_expenses
-projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+project_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 print("Your monthly savings are ${0}.".format(monthly_savings))
-print("Projected savings after one year, with interest, is: ${0}.".format(projected_savings))
+print("Projected savings after one year, with interest, is: ${0}.".format(project_savings))
