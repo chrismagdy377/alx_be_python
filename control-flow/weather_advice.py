43dae3aa0a54bfb3e2d79f3b27cbc1197ac77d68
chrismagdy377 <45969720+chrismagdy377@users.noreply.github.com>
2024-09-21 17:00:21 +0000

    Sat, Sep 21, 2024, 8:00 PM +03:00

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,4 +2,5 @@ weather = input("What's the weather like today? (sunny/rainy/cold):")
 if weather == "sunny"
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy"
-    print()
+    print("Don't forget your umbrella and a raincoat.")
+elif 
