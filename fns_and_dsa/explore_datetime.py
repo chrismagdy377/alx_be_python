9f81725176ffe91aac8474ade42e5345f7039f42
Christopher Magdy <chris.magdy37@gmail.com>
2024-09-29 20:55:20 +0000

    Sun, Sep 29, 2024, 11:55 PM +03:00

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,6 @@
 from datetime import timedelta
 
 def display_current_datetime(current_date):
-    print(datetime.now)
-    print("1")
-print(datetime)
-print("1")
+    current_date = datetime.datetime.now()
 
+print(current_date)
