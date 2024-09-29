3d407f8f5c83aea585009888d8497fba57523a53
Christopher Magdy <chris.magdy37@gmail.com>
2024-09-29 21:07:00 +0000

    Mon, Sep 30, 2024, 12:07 AM +03:00

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,5 +3,6 @@ import datetime
 def display_current_datetime(current_date):
     current_date = datetime.datetime.now()
     return current_date
+    print(current_date)
 
-display_current_datetime(current_date)
+display_current_datetime()
