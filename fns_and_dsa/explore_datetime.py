765fab8ac02dfbbc0306366f5380384528d0aef0
Christopher Magdy <chris.magdy37@gmail.com>
2024-09-30 14:19:04 +0300

    commit fix for date and time

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,25 +1,22 @@
 from datetime import datetime
 from datetime import timedelta
 
-
-current_date = datetime.now()
-
-def display_current_datetime(current_date):
+def display_current_datetime(current_date = datetime.now()):
     current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time: {0}".format(current_date))
     pass
 
-display_current_datetime(current_date)
+display_current_datetime()
 
-future_date = 0
-def calculate_future_date(future_date):
+current_date = datetime.now()
+def calculate_future_date(future_date = 0):
     future_date = int(input("Enter the number of days to add to the current date: "))
     future_date = current_date + timedelta(days=future_date)
     future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Future date: {0}".format(future_date))
     pass
 
-calculate_future_date(future_date)
+calculate_future_date()
 
 
 
