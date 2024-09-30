e70f9170cc51e14ad244e3a2fb2bac612d6c8ecd
Christopher Magdy <chris.magdy37@gmail.com>
2024-09-30 18:14:11 +0300

    last code edit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,13 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-temperature = int(input("Enter the temperature to convert: "))
+temp = input("Enter the temperature to convert: ")
+result = temp.isnumeric()
+if result == False:
+    print("Invalid temperature. Please enter a numeric value.")
+    exit()
+temperature = int(temp)
+
 temperature_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
 def convert_to_celsius(fahrenheit = 0):
@@ -17,6 +23,7 @@ def convert_to_fahrenheit(celsius = 0):
         print("{0}°C is {1}°F".format(temperature, celsius))
         return celsius
         pass
+
 
 convert_to_celsius()
 convert_to_fahrenheit()
